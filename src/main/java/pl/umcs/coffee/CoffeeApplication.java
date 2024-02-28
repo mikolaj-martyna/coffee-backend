107f2e191ab485795135e9c43c68952f6d25f573
mikolaj-martyna <mikolaj.martyna.01@gmail.com>
2024-02-28 17:14:46 +0100

    Initial commit

---
+++ src/main/java/pl/umcs/coffee/CoffeeApplication.java
@@ -0,0 +1,13 @@
+package pl.umcs.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/umcs/coffee/CoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umcs.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
