5c5e3af5c6d031b32feb810aea034957578e7518
mikolaj-martyna <mikolaj.martyna.01@gmail.com>
2024-03-07 12:01:23 +0100

    Add OrderController functionality

--- src/main/java/pl/umcs/coffee/order/OrderController.java
+++ src/main/java/pl/umcs/coffee/order/OrderController.java
@@ -1,10 +1,45 @@
 package pl.umcs.coffee.order;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("order")
 public class OrderController {
+    OrderServiceImpl orderService;
 
+    OrderController(OrderServiceImpl orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("create")
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+
+    @GetMapping("get/{orderId}")
+    public Order getOrder(@PathVariable Long orderId) {
+        return orderService.getOrder(orderId);
+    }
+
+    @GetMapping("get/user/{userId}")
+    public List<Order> getAllOrdersForUser(@PathVariable Long userId) {
+        return orderService.getAllOrdersForUser(userId);
+    }
+
+    @GetMapping("get/all")
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PutMapping("update")
+    public Order updateOrder(@RequestBody Order order) {
+        return orderService.updateOrder(order);
+    }
+
+    @DeleteMapping("delete/{orderId}")
+    public Order deleteOrder(@PathVariable Long orderId) {
+        return orderService.deleteOrder(orderId);
+    }
 }
