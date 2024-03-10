edf9d652124e8b9d94f09e44e25db457da2e71ae
mikolaj-martyna <mikolaj.martyna.01@gmail.com>
2024-03-10 11:50:55 +0100

    Fix User endpoints and http response codes

--- src/main/java/pl/umcs/coffee/user/UserServiceImpl.java
+++ src/main/java/pl/umcs/coffee/user/UserServiceImpl.java
@@ -26,15 +26,21 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(Long id) {
-        return userRepository.findById(id).orElse(null);
+        User foundUser = userRepository.findById(id).orElse(null);
+
+        if (foundUser == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return foundUser;
     }
 
     @Override
     public User updateUser(@NotNull User user) {
         User foundUser = userRepository.findById(user.getId()).orElse(null);
 
-        if (foundUser != null) {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        if (foundUser == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
         return userRepository.save(user);
@@ -45,7 +51,7 @@ public class UserServiceImpl implements UserService {
         User deletedUser = userRepository.findById(id).orElse(null);
 
         if (deletedUser == null) {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         userRepository.deleteById(id);
 
