c0587a4f5d5754ff96f76509a78667fe5427352d
mikolaj-martyna <mikolaj.martyna.01@gmail.com>
2024-06-15 14:48:21 +0200

    Begin PayU implementation

---
+++ src/main/java/pl/umcs/coffee/payment/PaymentController.java
@@ -0,0 +1,20 @@
+package pl.umcs.coffee.payment;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("payment")
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("authorize")
+    public void authorize() {
+        paymentService.authorize();
+    }
+}
